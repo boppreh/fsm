dea90b685ec5487df6b056de303b3e1fa5b23f8c
BoppreH <boppreh@gmail.com>
2013-09-05 22:16:38 -0300

    Add tuple implementation

--- fsm.py
+++ fsm.py
@@ -6,7 +6,17 @@ def fsm_from_dict(dictionary):
             state[symbol] = states[next_name]
     return states
 
+def fsm_from_tuples(tuples):
+    states = {name: {} for name, _, _ in tuples}
+    for cur_name, symbol, next_name in tuples:
+        states[cur_name][symbol] = states[next_name]
+    return states
+
 q0 = fsm_from_dict({'q0': {0: 'q0', 1: 'q1'},
                     'q1': {0: 'q0', 1: 'q1'}})['q0']
+t0 = fsm_from_tuples([('q0', 0, 'q0'), ('q0', 1, 'q1'),
+                      ('q1', 0, 'q0'), ('q1', 1, 'q1')])['q0']
+
 
 print q0[0][0][1][1][0] == q0
+print t0[0][0][1][1][0] == t0
