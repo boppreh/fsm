93a3cadd702e247d250c30ae861c7fecb5680536
BoppreH <boppreh@gmail.com>
2013-09-05 22:32:24 -0300

    Add FSM class

--- fsm.py
+++ fsm.py
@@ -12,11 +12,32 @@ def fsm_from_tuples(tuples):
         states[cur_name][symbol] = states[next_name]
     return states
 
-q0 = fsm_from_dict({'q0': {0: 'q0', 1: 'q1'},
-                    'q1': {0: 'q0', 1: 'q1'}})['q0']
-t0 = fsm_from_tuples([('q0', 0, 'q0'), ('q0', 1, 'q1'),
-                      ('q1', 0, 'q0'), ('q1', 1, 'q1')])['q0']
+class FSM(object):
+    def __init__(self, initial, rules, final, callbacks=()):
+        if isinstance(rules, dict):
+            self.states = fsm_from_dict(rules)
+        elif isinstance(rules, list):
+            self.states = fsm_from_tuples(rules)
+
+        self.state = self.states[initial]
+        self.final = set(id(self.states[name]) for name in final)
+        self.callbacks = {id(self.states[name]): fn for name, fn in callbacks}
+
+    def feed(self, symbol):
+        self.state = self.state[symbol]
+        s = id(self.state)
+        if s in self.callbacks:
+            self.callbacks[s]()
+        return self
+
+    def is_final(self):
+        return id(self.state) in self.final
 
 
-print q0[0][0][1][1][0] == q0
-print t0[0][0][1][1][0] == t0
+if __name__ == '__main__':
+    test_dict = {'q0': {0: 'q0', 1: 'q1'}, 'q1': {0: 'q0', 1: 'q1'}}
+    test_tuples = [('q0', 0, 'q0'), ('q0', 1, 'q1'),
+                   ('q1', 0, 'q0'), ('q1', 1, 'q1')]
+
+    f = FSM('q0', test_dict, ['q1'])
+    print f.feed(0).feed(0).feed(1).is_final()
